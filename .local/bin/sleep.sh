fbda34a71aeb5890468d360f301ac7cd46ee20c9
izmyname <135810812+izmyname@users.noreply.github.com>
2024-02-22 01:48:05 +0500

    remove -c option from swaylock

--- .local/bin/sleep.sh
+++ .local/bin/sleep.sh
@@ -1,4 +1,4 @@
-swayidle -w timeout 300 'swaylock -f -c 000000 -i $HOME/.local/share/lock.png' \
+swayidle -w timeout 300 'swaylock -f -i $HOME/.local/share/lock.png' \
 timeout 350 'hyprctl dispatch dpms off' \
 resume 'hyprctl dispatch dpms on' \
-before-sleep 'swaylock -f -c 000000 -i $HOME/.local/share/lock.png' &
+before-sleep 'swaylock -f -i $HOME/.local/share/lock.png' &
