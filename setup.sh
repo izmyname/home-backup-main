a252d7103cc9ac2dfc5df2c02982b86bc1a232f0
Izmyname <VI-MCMXCV@proton.me>
2024-03-01 14:53:31 +0500

    update setup script

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 
 #launch after copying home backup
 
-mkdir ~/app ~/doc ~/img ~/mus ~/vid ~/web ~/.config/mpd/playlists ~/.local/state ~/.local/state/mpd
+mkdir  ~/.config/mpd/playlists ~/.local/state ~/.local/state/mpd
 
 systemctl enable --now --user pipewire-pulse.service pipewire.service mpd.service
