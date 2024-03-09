584bdf5cfc8e3dcedb3f8c7f611c0a7441d69db6
Izmyname <VI-MCMXCV@proton.me>
2024-03-09 11:45:41 +0500

    update root backup 3

--- root-backup.sh
+++ root-backup.sh
@@ -3,4 +3,4 @@
 #This a root backup script. Copy it to the / directory, make it executable and lauch as root. Edit the script to add new config files for backup.
 #Dependencies: atool, tar
 
-apack arch_system_config-ddmmyy.tar.gz etc/environment etc/fstab etc/hostname etc/kernel/cmdline etc/locale.conf /etc/locale.gen etc/localtime etc/mkinitcpio.conf etc/mkinitcpio.d etc/modprobe.d etc/pacman.conf etc/pacman.d/hooks etc/pkglist.txt etc/sddm.conf etc/shells etc/snapper etc/sysctl.d etc/systemd/system etc/tlp.conf /usr/share/backgrounds/sddm-login.png /usr/share/sddm/themes/slice/theme.conf.user etc/adjtime
+apack arch_system_config-ddmmyy.tar.gz etc/environment etc/fstab etc/hostname etc/kernel/cmdline etc/locale.conf /etc/locale.gen etc/localtime etc/mkinitcpio.conf etc/mkinitcpio.d etc/modprobe.d etc/pacman.conf etc/pacman.d/hooks etc/pkglist.txt etc/sddm.conf etc/snapper etc/sysctl.d etc/systemd/system etc/tlp.conf /usr/share/backgrounds/sddm-login.png /usr/share/sddm/themes/slice/theme.conf.user etc/adjtime etc/sudoers
