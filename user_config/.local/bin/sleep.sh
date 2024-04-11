218350c46a75a3fbf0351e7551016e12d49d99b7
Izmyname <VI-MCMXCV@proton.me>
2024-04-11 16:56:25 +0500

    replace swaylock with hyprlock

--- user_config/.local/bin/sleep.sh
+++ user_config/.local/bin/sleep.sh
@@ -1,4 +1,4 @@
-swayidle -w timeout 300 'swaylock -f -i $HOME/.local/share/lock.png' \
+swayidle -w timeout 300 'hyprlock' \
 timeout 350 'hyprctl dispatch dpms off' \
 resume 'hyprctl dispatch dpms on' \
-before-sleep 'swaylock -f -i $HOME/.local/share/lock.png' &
+before-sleep 'hyprlock' &
