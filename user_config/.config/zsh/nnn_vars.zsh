81aaa67b3f3c5747e21edccae077be86301cf6f7
izmyname <135810812+izmyname@users.noreply.github.com>
2024-04-14 17:34:24 +0500

    use bat instead of less

--- user_config/.config/zsh/nnn_vars.zsh
+++ user_config/.config/zsh/nnn_vars.zsh
@@ -1,6 +1,6 @@
 export NNN_TERMINAL='foot -T preview-tui'
 export NNN_BMS='c:~/.config/;o:~/.local/opt;l:~/.local/share;'
-export NNN_PLUG='m:mtpmount;p:preview-tui;f:fzopen;M:nmount;S:suedit;u:getplugs;g:gpge;G:gpgd;i:-!|mediainfo "$nnn";l:-!less -iR "$nnn"*;x:!&gio launch "$nnn";v:-!&swayimg "$nnn";s:-!dua "$nnn";w:-!&mpv "$nnn";a:autojump;'
+export NNN_PLUG='m:mtpmount;p:preview-tui;f:fzopen;M:nmount;S:suedit;u:getplugs;g:gpge;G:gpgd;i:-!|mediainfo "$nnn";l:-!bat --paging=always "$nnn"*;x:!&gio launch "$nnn";v:-!&swayimg "$nnn";s:-!dua "$nnn";w:-!&mpv "$nnn";a:autojump;'
 export NNN_TRASH=1
 BLK="04" CHR="04" DIR="04" EXE="00" REG="00" HARDLINK="00" SYMLINK="06" MISSING="00" ORPHAN="01" FIFO="0F" SOCK="0F" OTHER="02"
 export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
