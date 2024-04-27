853908390356756d9dd5c8ddbc4087be821e1ca9
izmyname <135810812+izmyname@users.noreply.github.com>
2024-04-27 21:40:12 +0500

    Change colorscheme

--- user_config/.config/zsh/nnn_vars.zsh
+++ user_config/.config/zsh/nnn_vars.zsh
@@ -2,7 +2,8 @@ export NNN_TERMINAL='foot -T preview-tui'
 export NNN_BMS='c:~/.config/;o:~/.local/opt;l:~/.local/share;'
 export NNN_PLUG='m:mtpmount;p:preview-tui;f:fzopen;M:nmount;S:suedit;u:getplugs;g:gpge;G:gpgd;i:-!|mediainfo "$nnn";l:-!bat --paging=always "$nnn"*;x:!&gio launch "$nnn";v:-!&swayimg "$nnn";s:-!dua "$nnn";w:-!&mpv "$nnn";a:autojump;'
 export NNN_TRASH=1
-BLK="04" CHR="04" DIR="04" EXE="00" REG="00" HARDLINK="00" SYMLINK="06" MISSING="00" ORPHAN="01" FIFO="0F" SOCK="0F" OTHER="02"
-export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
+BLK="03" CHR="03" DIR="04" EXE="02" REG="07" HARDLINK="05" SYMLINK="05" MISSING="08" ORPHAN="01" FIFO="06" SOCK="03" UNKNOWN="01"
+export NNN_COLORS="#04020301;4231"
+export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$UNKNOWN"
 export NNN_ARCHIVE="\\.(7z|a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|rar|rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)$"
 export NNN_FIFO='/tmp/nnn.fifo'
