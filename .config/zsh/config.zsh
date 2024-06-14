f6d1835092429a6a938b23fa0b4c7bd87d18d730
Izmyname <VI-MCMXCV@proton.me>
2024-06-14 19:20:25 +0500

    add fancy prompt

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -10,4 +10,4 @@ setopt autocd extendedglob notify correct
 unsetopt beep
 bindkey -v
 autoload -U colors && colors
-PS1="%{$fg[magenta]%}%D{%a %d %b} %D{%H:%M} %{$reset_color%}%{$fg[cyan]%}%n%{$reset_color%} %{$fg[blue]%}%~ %{$reset_color%}%{$fg[white]%}>%{$reset_color%}% "
+PS1="%B%{$fg[magenta]%}%  %D{%a %d %b} %D{%H:%M} %{$reset_color%}%b%B%{$fg[cyan]%}%  %n%{$reset_color%}%b %B%{$fg[blue]%}% 📂%~ %{$reset_color%}%b%{$fg[white]%}>%{$reset_color%}% "
