329df3e325731b1879e245a184c4c731300f4447
Izmyname <VI-MCMXCV@proton.me>
2024-07-29 13:02:52 +0500

    enable some lazyvim plugins

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
-  { "nvim-telescope/telescope.nvim" },
-  branch = "0.1.x",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  { "nvim-telescope/telescope-fzf-native.nvim" },
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  -- or                              , branch = '0.1.x',
+  dependencies = { "nvim-telescope/telescope-fzf-native.nvim" },
+  { "nvim-lua/plenary.nvim" },
 }
