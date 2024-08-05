ccc26261c3a241066ff9f612335a8904480ce5f6
Izmyname <VI-MCMXCV@proton.me>
2024-08-05 22:14:31 +0500

    open swayimg in gallery mod with the nnn keybind

--- .config/zsh/nnn_vars.zsh
+++ .config/zsh/nnn_vars.zsh
@@ -2,7 +2,7 @@ export NNN_OPTS='ex'
 export NNN_TERMINAL='foot -T preview-tui'
 export NNN_ICONLOOKUP=1
 export NNN_BMS='c:~/.config/;o:~/.local/opt;l:~/.local/share;'
-export NNN_PLUG='m:mtpmount;p:preview-tui;f:fzopen;M:nmount;S:suedit;u:getplugs;g:gpge;G:gpgd;i:-!|mediainfo "$nnn";l:-!bat --paging=always "$nnn"*;x:!&gio launch "$nnn";v:-!&swayimg "$nnn";s:-!dua "$nnn";w:-!&mpv "$nnn";a:autojump;'
+export NNN_PLUG='m:mtpmount;p:preview-tui;f:fzopen;M:nmount;S:suedit;u:getplugs;g:gpge;G:gpgd;i:-!|mediainfo "$nnn";l:-!bat --paging=always "$nnn"*;x:!&gio launch "$nnn";v:-!&swayimg -g "$nnn";s:-!dua "$nnn";w:-!&mpv "$nnn";a:autojump;'
 export NNN_TRASH=1
 BLK="03" CHR="03" DIR="04" EXE="02" REG="07" HARDLINK="05" SYMLINK="05" MISSING="08" ORPHAN="01" FIFO="06" SOCK="03" UNKNOWN="01"
 export NNN_COLORS="#04020301;4231"
